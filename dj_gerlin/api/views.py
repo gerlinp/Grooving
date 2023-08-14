47bd4b4c76a3c6c081d3a5d72cb0641d49176785
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-08-14 19:51:59 -0400

    Update views.py

--- dj_gerlin/api/views.py
+++ dj_gerlin/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
 
-def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
