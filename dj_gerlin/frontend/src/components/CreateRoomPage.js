bd552b47d992b7f96dad45b2c668983b9f9c2801
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-08-27 12:07:08 -0400

    update

--- dj_gerlin/frontend/src/components/CreateRoomPage.js
+++ dj_gerlin/frontend/src/components/CreateRoomPage.js
@@ -11,13 +11,19 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 
 export default class CreateRoomPage extends Component {
-  defaultVotes = 2
+  static defaultProps = {
+    votesToSkip: 2,
+    guestCanPause: true,
+    update: false,
+    roomCode: null,
+    updateCallback: () => {},
+  }
 
   constructor(props) {
     super(props)
     this.state = {
-      guestCanPause: true,
-      votesToSkip: this.defaultVotes,
+      guestCanPause: this.props.guestCanPause,
+      votesToSkip: this.props.votesToSkip,
     }
 
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this)
@@ -51,12 +57,41 @@ export default class CreateRoomPage extends Component {
       .then((data) => this.props.history.push('/room/' + data.code))
   }
 
+  renderCreateButtons() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align='center'>
+          <Button color='primary' variant='contained' onClick={this.handleRoomButtonPressed}>
+            Create A Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align='center'>
+          <Button color='secondary' variant='contained' to='/' component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  renderUpdateButtons() {
+    return (
+      <Grid item xs={12} align='center'>
+        <Button color='primary' variant='contained' onClick={this.handleRoomButtonPressed}>
+          Update Room
+        </Button>
+      </Grid>
+    )
+  }
+
   render() {
+    const title = this.props.update ? 'Update Room' : 'Create A Room'
+
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align='center'>
           <Typography component='h4' variant='h4'>
-            Create A Room
+            {title}
           </Typography>
         </Grid>
         <Grid item xs={12} align='center'>
@@ -86,7 +121,7 @@ export default class CreateRoomPage extends Component {
               required={true}
               type='number'
               onChange={this.handleVotesChange}
-              defaultValue={this.defaultVotes}
+              defaultValue={this.state.votesToSkip}
               inputProps={{
                 min: 1,
                 style: { textAlign: 'center' },
