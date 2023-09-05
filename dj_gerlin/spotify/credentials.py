5d1f6bb4edf692c5824c0d110d1a106a23227e49
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-09-05 16:49:23 -0400

    Spotify API
    Still need to debug to figure out issues

--- dj_gerlin/spotify/credentials.py
+++ dj_gerlin/spotify/credentials.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-REDIRECT_URI = ''
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
