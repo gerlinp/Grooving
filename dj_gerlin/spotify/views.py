b39a03b33fac381e1815a08c8d22eb1b4931eeef
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-09-20 23:25:36 -0400

    Send request, Execute api call, send a post request

--- dj_gerlin/spotify/views.py
+++ dj_gerlin/spotify/views.py
@@ -54,3 +54,19 @@ class IsAuthenticated(APIView):
         is_authenticated = is_spotify_authenticated(
             self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        if room.exists():
+            room = room[0]
+        else:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        host = room.host
+        endpoint = 'player/currently-playing'
+        response = execute_spotify_api_request(host, endpoint)
+        print(response)
+
+        return Response(response, status=status.HTTP_200_OK)
+    
