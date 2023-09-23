99b7f13ba6fda583f1094696dc4111b6686bdbf3
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-09-22 21:10:43 -0400

    post request

--- dj_gerlin/frontend/src/components/Room.js
+++ dj_gerlin/frontend/src/components/Room.js
@@ -11,6 +11,7 @@ export default class Room extends Component {
       isHost: false,
       showSettings: false,
       spotifyAuthenticated: false,
+      song: {}
     }
     this.roomCode = this.props.match.params.roomCode
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this)
@@ -20,6 +21,7 @@ export default class Room extends Component {
     this.getRoomDetails = this.getRoomDetails.bind(this)
     this.authenticateSpotify = this.authenticateSpotify.bind(this)
     this.getRoomDetails()
+    this.getCurrentSong()
   }
 
   getRoomDetails() {
@@ -57,6 +59,16 @@ export default class Room extends Component {
             })
         }
       })
+  }
+
+  getCurrentSong() {
+    fetch('/spotify/current-song').then((response) => {
+      if (!response.ok) {
+        return {};
+      } else {
+        response.json();
+      }
+    }).then((data) => this.setState({ song: data }))
   }
 
   leaveButtonPressed() {
@@ -122,21 +134,7 @@ export default class Room extends Component {
             Code: {this.roomCode}
           </Typography>
         </Grid>
-        <Grid item xs={12} align='center'>
-          <Typography variant='h6' component='h6'>
-            Votes: {this.state.votesToSkip}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align='center'>
-          <Typography variant='h6' component='h6'>
-            Guest Can Pause: {this.state.guestCanPause.toString()}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align='center'>
-          <Typography variant='h6' component='h6'>
-            Host: {this.state.isHost.toString()}
-          </Typography>
-        </Grid>
+        {this.state.song}
         {this.state.isHost ? this.renderSettingsButton() : null}
         <Grid item xs={12} align='center'>
           <Button variant='contained' color='secondary' onClick={this.leaveButtonPressed}>
