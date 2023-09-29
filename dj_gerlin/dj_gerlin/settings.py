93247fa8471468f70a5b6e8f2ea40f1168523122
Gerlin <68314210+gerlinp@users.noreply.github.com>
2023-09-29 14:07:35 -0400

    heroku deploy settings

--- dj_gerlin/dj_gerlin/settings.py
+++ dj_gerlin/dj_gerlin/settings.py
@@ -1,3 +1,8 @@
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
 """
 Django settings for dj_gerlin project.
 
@@ -51,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dj_gerlin.urls'
@@ -121,7 +127,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
